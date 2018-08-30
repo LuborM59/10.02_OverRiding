46754cdbeab02ee0bfb1e03aca235744a6c84929
Lubor Martinský <lubor.martinsky@hotmail.com>
2018-08-30 15:49:34 +0200

    Add project files.

---
+++ 10.02_OverRiding/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._02_OverRiding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fish fish = new Fish();
+            Cat cat = new Cat();
+
+            Console.WriteLine("Ryba: "+fish.makeSound());
+            Console.WriteLine("Cica: " + cat.makeSound());
+
+        }
+
+        class Animal
+        {
+            public virtual string makeSound()
+            {
+                return "";
+            }
+        }
+
+        class Fish : Animal
+        {
+
+        }
+
+        class Cat : Animal
+        {
+            public override string makeSound()
+            {
+                return "Mnau";
+            }
+        }
+    }
+}
